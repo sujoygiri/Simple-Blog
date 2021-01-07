9570492f4b53af8380efbe69bb6b4873fd95f302
sujoy giri <girisujoy630@gmail.com>
2021-01-07 13:58:54 +0530

    creatin forms.py

--- FirstBlog/BlogApp/models.py
+++ FirstBlog/BlogApp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+class SuperUser(models.Model):
+
+    pass
+
+class BlogPost(models.Model):
+
+    Author = models.ForeignKey('auth.User')
+    Title = models.CharField(max_length=200,blank=False)
+    PostContent = models.TextField(blank=False)
+    CreationDate = models.DateTimeField(default=timezone.now())
+    PublishDate = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.PublishDate = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(ApprovedComment=True)
+
+    def __str__(self):
+        return self.Title
+
+class Comments(models.Model):
+
+    Post = models.ForeignKey('BlogApp.BlogPost',related_name='comment')
+    UserName = models.CharField(max_length=50)
+    Text = models.TextField()
+    CreationDate = models.DateTimeField(default=timezone.now())
+    ApprovedComment = models.BooleanField(default=False)
+    
+    def approve(self):
+        self.ApprovedComment = True
+        self.save()
+
+    def __str__(self):
+        return self.Text
