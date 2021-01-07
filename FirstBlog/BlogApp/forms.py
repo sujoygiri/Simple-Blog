21ecde33de3570935b59f8f1a57e5d7a7276fda9
sujoy giri <girisujoy630@gmail.com>
2021-01-07 14:47:44 +0530

    creating form

--- FirstBlog/BlogApp/forms.py
+++ FirstBlog/BlogApp/forms.py
@@ -1,3 +1,8 @@
 from django import forms
 from .models import BlogPost,Comments
 
+class BlogPostForm(forms.ModelForm):
+
+    class Meta():
+        model = BlogPost
+        field = {'Author','Title','PostContent'}
