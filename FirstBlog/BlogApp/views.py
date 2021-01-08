9bc6fe8329c5044a0f55c80d0a31eae3ae4b7021
sujoy giri <girisujoy630@gmail.com>
2021-01-08 11:21:41 +0530

    sorting import

--- FirstBlog/BlogApp/views.py
+++ FirstBlog/BlogApp/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import BlogPostForm,CommentsForm
-from django.utils import timezone
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
-from .models import BlogPost,Comments
+from django.utils import timezone
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  TemplateView, UpdateView)
+
+from .forms import BlogPostForm, CommentsForm
+from .models import BlogPost, Comments
+
 # Create your views here.
 LogInUrl = '/login/'
 
